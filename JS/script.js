c91bfd88aa873ef9d3931468aba05c9f4ae2f1cd
Gustavo Henrique <gustavomirandaoliveira@outlook.com>
2025-11-05 00:36:02 -0300

    Adicionando redirecionamento links

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function redirectWithAlert(url, siteName) {
+    Swal.fire({
+        title: 'Redirecionamento',
+        text: `Você está sendo redirecionado para ${siteName}. Deseja continuar?`,
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonText: 'Sim, continuar',
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        backdrop: true,
+        allowOutsideClick: false,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.open(url, '_blank');
+        }
+    });
+}
